d973a16b8d85e880b167b0692f5808022922a222
Serafimka01 <okunevaserafima37@gmail.com>
2024-05-19 23:19:38 +0300

    Урок 2: ДЗ после 4ой части

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -25,4 +25,20 @@ public record Triangle(
         double p = (this.sideA + this.sideB + this.sideC) / 2;
         return Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare(sideA, triangle.sideA) == 0 && Double.compare(sideB, triangle.sideB) == 0 && Double.compare(sideC, triangle.sideC) == 0
+                || Double.compare(sideA, triangle.sideB) == 0 && Double.compare(sideB, triangle.sideC) == 0 && Double.compare(sideC, triangle.sideA) == 0
+                || Double.compare(sideA, triangle.sideC) == 0 && Double.compare(sideB, triangle.sideA) == 0 && Double.compare(sideC, triangle.sideB) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -36,4 +36,18 @@ public class TriangleTests {
             //ok
         }
     }
+
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(3.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testIsoscelesTriangle() {
+        var t1 = new Triangle(4.0, 4.0, 5.0);
+        var t2 = new Triangle(4.0, 5.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
 }
