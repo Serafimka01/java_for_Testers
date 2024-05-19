2def2ee6ad763f796356d6e132f77681f8bbf428
Serafimka01 <okunevaserafima37@gmail.com>
2024-05-19 23:13:05 +0300

    Урок 2: Созданы тесты для прямоугольника

---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTests.java
@@ -0,0 +1,30 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RectangleTests {
+    @Test
+    void cannotCreateRectangleWithNegativeSide() {
+        try {
+            new Rectangle(-5.0, 3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException ex) {
+            //ok
+        }
+    }
+
+    @Test
+    void testEquality() {
+        var r1 = new Rectangle(5.0, 6.0);
+        var r2 = new Rectangle(5.0, 6.0);
+        Assertions.assertEquals(r1, r2);
+    }
+
+    @Test
+    void testEquality2() {
+        var r1 = new Rectangle(6.0, 5.0);
+        var r2 = new Rectangle(5.0, 6.0);
+        Assertions.assertEquals(r1, r2);
+    }
+}
